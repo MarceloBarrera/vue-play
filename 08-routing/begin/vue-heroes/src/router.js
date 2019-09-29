b6cb2f1bf704c1c354bd517b89f00cc354269254
Marcelo Barrera <marcelo.barrera@confirmit.com>
2019-09-29 16:49:50 +0200

    setting lazy load bundles with webpack and on router

--- 08-routing/begin/vue-heroes/src/router.js
+++ 08-routing/begin/vue-heroes/src/router.js
@@ -1,11 +1,11 @@
+/* eslint-disable */
 import Vue from 'vue';
 import Router from 'vue-router';
-import Heroes from './views/heroes.vue';
-import HeroDetail from './views/hero-detail.vue';
-
+import PageNotFound from './views/page-not-found';
 Vue.use(Router);
 
 const parseProps = r => ({ id: parseInt(r.params.id) });
+
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -17,12 +17,16 @@ export default new Router({
     {
       path: '/heroes',
       name: 'heroes',
-      component: Heroes,
+      component: () =>
+        import(/* webpackChunkName: "bundle-heroes" */ './views/heroes.vue'),
     },
     {
       path: '/heroes/:id',
       name: 'hero-detail',
-      component: HeroDetail,
+      component: () =>
+        import(
+          /*webpackChunkName: "bundle-hero-detail"*/ './views/hero-detail.vue'
+        ),
       props: parseProps,
     },
     {
@@ -31,8 +35,13 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
+      //webpackChunkName is for webpack, if this is not there webpack pick up random name
       component: () =>
-        import(/* webpackChunkName: "about" */ './views/about.vue'),
+        import(/* webpackChunkName: "bundle-about" */ './views/about.vue'),
+    },
+    {
+      path: '*',
+      component: PageNotFound,
     },
   ],
 });

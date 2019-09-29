d45ac71838107b870fc6b9bd5af1d38f49f90797
Marcelo Barrera <marcelo.barrera@confirmit.com>
2019-09-29 17:34:37 +0200

    iitial dummy state and several ways to mapstate

--- 09-managing-state/begin/vue-heroes/src/store/index.js
+++ 09-managing-state/begin/vue-heroes/src/store/index.js
@@ -3,7 +3,18 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-const state = {};
+const state = {
+  heroes: [
+    {
+      id: 20,
+      firstName: 'pepe',
+      lastName: 'batman',
+      capeCounter: 3,
+      originDate: '08/01/1998',
+      description: 'batman pepe',
+    },
+  ],
+};
 const mutations = {};
 const actions = {};
 const getters = {};

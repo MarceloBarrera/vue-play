4d0591616cae1d47ff316aec6bb04fbddabe038a
Marcelo Barrera <marcelo.barrera@confirmit.com>
2019-09-29 21:18:40 +0200

    adding mutations and mapactions

--- 09-managing-state/begin/vue-heroes/src/store/index.js
+++ 09-managing-state/begin/vue-heroes/src/store/index.js
@@ -1,22 +1,27 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { dataService } from '../shared';
+import { GET_HEROES } from './mutation-types';
 
 Vue.use(Vuex);
 
 const state = {
-  heroes: [
-    {
-      id: 20,
-      firstName: 'pepe',
-      lastName: 'batman',
-      capeCounter: 3,
-      originDate: '08/01/1998',
-      description: 'batman pepe',
-    },
-  ],
+  heroes: [],
 };
-const mutations = {};
-const actions = {};
+
+const mutations = {
+  [GET_HEROES](state, heroes) {
+    state.heroes = heroes;
+  },
+};
+
+const actions = {
+  async getHeroesAction({ commit }) {
+    const heroes = await dataService.getHeroes();
+    commit(GET_HEROES, heroes);
+  },
+};
+
 const getters = {};
 
 export default new Vuex.Store({
